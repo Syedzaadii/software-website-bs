0303c7b40457e6bf9391b6a042424340072386f6
Syeda Sundas <ssyedabukharii@gmail.com>
2024-09-25 17:49:14 +0500

    Add initial files

---
+++ js/script.js
@@ -0,0 +1,29 @@
+const checkReplace = document.querySelector(".replaceme");
+
+if (checkReplace !== null) {
+  const replace = new ReplaceMe(checkReplace, {
+    animation: "animated fadeIn",
+    speed: 2000,
+    separator: ",",
+    loopCount: "infinite",
+    autoRun: true,
+  });
+}
+function userScroll() {
+  const navbar = document.querySelector(".navbar");
+
+  window.addEventListener("scroll", () => {
+    if (window.scrollY > 50) {
+      navbar.classList.add("bg-dark");
+      navbar.classList.add("border-bottom");
+      navbar.classList.add("border-secondary");
+
+      navbar.classList.add("navbar-sticky");
+    } else {
+      navbar.classList.remove("bg-dark");
+      navbar.classList.remove("border-bottom");
+      navbar.classList.remove("border-secondary");
+      navbar.classList.remove("navbar-sticky");
+    }
+  });
+}
